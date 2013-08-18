704e259614e405e6de296ef72579a577f29a0126
Markus Fenske <iblue@gmx.net>
2013-08-18 13:04:28 +0200

    Included cache middleware into rack stack

--- cache.rb
+++ cache.rb
@@ -1,2 +1,12 @@
+# Middleware that caches all pages
 class Cache
+  def initialize(appl)
+    @appl = appl
+  end
+
+  def call(env)
+    status, headers, body = @appl.call(env)
+
+    [status, headers, body + ["<p>(went through cache middleware)</p"]]
+  end
 end
--- config.ru
+++ config.ru
@@ -1,5 +1,7 @@
+require './cache'
 require './router'
 
 use Rack::Reloader # Reload stuff automatically (TODO: testing environment
                    # only)
+use Cache
 run Router.new
