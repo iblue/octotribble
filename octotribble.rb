c81a4a67571dc11e66e7b4cf6d76d39d7f227a54
Markus Fenske <iblue@gmx.net>
2013-09-08 13:37:17 +0200

    Moved files around

--- octotribble.rb
+++ octotribble.rb
@@ -1,6 +1,24 @@
 require 'sinatra/asset_pipeline'
 
 class Octotribble < Sinatra::Base
+   # Include these files when precompiling assets
+  set :assets_precompile, %w(main.css *.png *.jpg *.svg *.eot *.ttf *.woff)
+
+  # Logical path to your assets
+  set :assets_prefix, 'assets'
+
+  # Use another host for serving assets
+  #set :asset_host, 'http://<id>.cloudfront.net'
+
+  # Serve assets using this protocol
+  set :assets_protocol, :http
+
+  # CSS minification
+  set :assets_css_compressor, :sass
+
+  # JavaScript minification
+  set :assets_js_compressor, :uglifier
+
   register Sinatra::AssetPipeline
 
   set :views, ['views', 'content']
