bc619b4d70736da975bec00e64fcb5be637ceb1c
Markus Fenske <iblue@gmx.net>
2013-09-08 11:27:26 +0200

    Markdown and layout

--- main.rb
+++ main.rb
@@ -7,6 +7,17 @@ else
   Bundler.require(:default, :development)
 end
 
-get '/hi' do
-  "Hello World!"
+set :views, ['views', 'content']
+
+helpers do
+  def find_template(views, name, engine, &block)
+    Array(views).each { |v| super(v, name, engine, &block) }
+  end
+end
+
+# for all markdown files, use post.haml as layout
+set :markdown, :layout_engine => :haml, :layout => :post
+
+get '/' do
+  markdown :index
 end
